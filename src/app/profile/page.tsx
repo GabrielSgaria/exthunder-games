17ba06f07358909ff1f7407800f12ec533d2220e
Gabriel Sgaria <gabrielsgaria2@gmail.com>
2024-06-24 15:59:22 -0300

    feat: concluded

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -2,6 +2,7 @@ import { Container } from "@/components/container";
 import Image from "next/image";
 import userImg from '/public/user.png'
 import { FaShareAlt } from "react-icons/fa";
+import { FavoriteCard } from "@/components/favorite";
 
 export default function Profile() {
     return (
@@ -28,6 +29,18 @@ export default function Profile() {
                     </div>
                 </section>
 
+        <section className="flex flex-wrap gap-5 flex-col md:flex-row">
+            <div className="flex-grow flex-wrap">
+                <FavoriteCard />
+            </div>
+            <div className="flex-grow flex-wrap">
+                <FavoriteCard />
+            </div>
+            <div className="flex-grow flex-wrap">
+                <FavoriteCard />
+            </div>
+        </section>
+        
             </Container>
         </main>
     )
